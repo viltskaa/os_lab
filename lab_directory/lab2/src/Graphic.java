d1a21b7d5e3ac80498ce15846a595873fa6fa16f
Viltskaa <andrewstyou@gmail.com>
2022-10-11 00:31:00 +0400

    10 10 22

--- lab_directory/lab2/src/Graphic.java
+++ lab_directory/lab2/src/Graphic.java
@@ -48,41 +48,4 @@ public class Graphic extends JFrame {
       Y -= height;
     }
   }
-
-//  @Override
-//  public void paiknt(Graphics graphics) {
-//    super.paint(graphics);
-//    int count = scheduler.Length();
-//    int StartX = 0;
-//    for (int i = 0; i < count; i++) {
-//      Process object = scheduler.GetProcessByIndex(i);
-//      graphics.setColor(colors[i]);
-//      double width = Math.floor(((double)getWidth() / scheduler.GetAllTime()) * object.GetTime());
-//      graphics.fillRect(StartX, 0, (int)width, getHeight());
-//
-//      Font font = graphics.getFont();
-//      AffineTransform fontAT = new AffineTransform();
-//      fontAT.rotate(Math.PI / 2);
-//      graphics.setColor(Color.white);
-//      graphics.setFont(font.deriveFont(fontAT));
-//      graphics.drawString(object.GetName(), StartX + 10, 40);
-//      graphics.setFont(font);
-//
-//      int StartXforThreads = StartX;
-//      int countOnY = getHeight() / object.GetLength();
-//      graphics.setColor(Color.BLACK);
-//      for (int j = 0; j < object.GetLength(); j++) {
-//        double widthThread = Math.floor((width / object.GetTime()) * object.GetThreadByIndex(j).getTime());
-//        graphics.drawRect(
-//            StartXforThreads,
-//            countOnY * j,
-//            (int)widthThread,
-//            countOnY
-//        );
-//        StartXforThreads += widthThread;
-//      }
-//
-//      StartX += width;
-//    }
-//  }
 }
